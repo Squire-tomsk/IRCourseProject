229b8d04c9c16f48ce655b7841caac823ca78c1e
abuca <abuca9268@gmail.com>
2016-11-06 23:18:57 +0300

    RedisDocumentDAO bug fix.

--- src/main/scala/DAO/imp/redis/RedisDocumentDAO.scala
+++ src/main/scala/DAO/imp/redis/RedisDocumentDAO.scala
@@ -7,13 +7,14 @@ import DAO.traits.DocumentDAO
   * Created by abuca on 04.11.16.
   */
 class RedisDocumentDAO extends DocumentDAO {
-  val docKeyPrefix: String = "document:docId:"
+  val docKeyTextPrefix: String = "document:text:docId:"
+  val docKeyUrlPrefix: String = "document:url:docId:"
 
   override def setDocument(docId: Long, text: String, url: String) = {
     BasicDAO.redisConnectionPool.withClient {
       client => {
-        client.set(docKeyPrefix + docId, text)
-        client.set(docKeyPrefix + docId + ":url", text)
+        client.set(docKeyTextPrefix + docId, text)
+        client.set(docKeyUrlPrefix + docId, url)
       }
     }
   }
@@ -21,7 +22,7 @@ class RedisDocumentDAO extends DocumentDAO {
   override def getDocument(docId: Long): String = {
     BasicDAO.redisConnectionPool.withClient {
       client => {
-        client.get(docKeyPrefix + docId).getOrElse("")
+        client.get(docKeyTextPrefix + docId).getOrElse("")
       }
     }
   }
@@ -30,7 +31,7 @@ class RedisDocumentDAO extends DocumentDAO {
   override def getUrl(docId: Long): String = {
     BasicDAO.redisConnectionPool.withClient {
       client => {
-        client.get(docKeyPrefix + docId + ":url").getOrElse("")
+        client.get(docKeyUrlPrefix + docId).getOrElse("")
       }
     }
   }
@@ -38,7 +39,7 @@ class RedisDocumentDAO extends DocumentDAO {
   override def getStoredDocumentCount: Long = {
     BasicDAO.redisConnectionPool.withClient {
       client => {
-        val keys = client.keys(docKeyPrefix + "*").getOrElse(List())
+        val keys = client.keys(docKeyTextPrefix + "*").getOrElse(List())
         keys.length.toLong
       }
     }
@@ -47,7 +48,10 @@ class RedisDocumentDAO extends DocumentDAO {
   override def erace() = {
     BasicDAO.redisConnectionPool.withClient {
       client => {
-        val keys = client.keys(docKeyPrefix + "*").getOrElse(List())
+        client.keys(docKeyTextPrefix + "*").getOrElse(List())
+          .map({ key => key.getOrElse("") })
+          .foreach(key => client.del(key))
+        client.keys(docKeyUrlPrefix + "*").getOrElse(List())
           .map({ key => key.getOrElse("") })
           .foreach(key => client.del(key))
       }
