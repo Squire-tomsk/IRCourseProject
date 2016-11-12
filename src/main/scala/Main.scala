eafd9a2097af9e39029ba31cc824a1e374af1b48
abuca <abuca9268@gmail.com>
2016-11-13 00:02:24 +0300

    Simple search engine is added.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,14 +3,34 @@ import DAO.BasicDAO
 import DAO.imp.postgres.PostgresDocumentDAO
 import DAO.imp.redis.BashOrgCrawlerDAO
 import crawlers.BashOrgCrawler
-import structures.Dictionary
-import utils.TermExtractor
+import engine.SimpleSearchEngine
+import structures.{Dictionary, PostingList}
+import utils.{DocToVecConverter, TermExtractor}
 
 object Main {
   def main(args: Array[String]) {
     BasicDAO.init()
 
-    fillDictionary
+    example
+  }
+
+  def example: Unit = {
+    val engine = new SimpleSearchEngine
+    val documentDAO = new PostgresDocumentDAO
+    val extractor = new TermExtractor
+    val random = scala.util.Random
+
+    val docID = math.abs(random.nextLong()) % documentDAO.getStoredDocumentCount
+    val doc = documentDAO.getDocument(docID)
+
+    val terms = extractor.extract(doc)
+    val query = (1 to 3).
+      map(i => random.nextInt(terms.length-1)).
+      map(n => terms.apply(n)).
+      reduce((term1,term2) => term1 + " " + term2)
+
+    println(docID)
+    println(engine.search(query))
   }
 
   def crawlDocsFromBashOrg : Unit = {
@@ -22,6 +42,18 @@ object Main {
     val crawler = new BashOrgCrawler
     crawler begin
   }
+
+  def extractPostingLists : Unit = {
+    val postingLists = new PostingList
+    val extractor = new TermExtractor
+    val documentDAO = new PostgresDocumentDAO
+
+    val range = 1 to documentDAO.getStoredDocumentCount.toInt
+    range.map(id => (id,documentDAO.getDocument(id))).
+      map(doc => (doc._1,extractor.extract(doc._2))).
+      foreach(doc => postingLists.add(doc._2,doc._1))
+  }
+
 
   def fillDictionary(): Unit ={
     val extractor = new TermExtractor
---
+++ src/main/scala/engine/SimpleSearchEngine.scala
@@ -0,0 +1,37 @@
+package engine
+
+import DAO.imp.postgres.PostgresDocumentDAO
+import structures.{Dictionary, PostingList}
+import utils.{DocToVecConverter, TermExtractor}
+
+/**
+  * Created by abuca on 12.11.16.
+  */
+class SimpleSearchEngine {
+  val documentDAO = new PostgresDocumentDAO
+  val postingLists = new PostingList
+  val converter = new DocToVecConverter
+  val extractor = new TermExtractor
+  val dictionary = new Dictionary
+
+  def search(query: String): List[(Long,Double)] = {
+    val terms = extractor.extract(query)
+
+    val docPool = terms.
+      map(term => dictionary.getDocIDSet(term)).
+      reduce((set1,set2) => set1.union(set2)).
+      toList
+
+    val queryVector = converter.
+      convert(query).
+      map(tfEntety => (tfEntety._1,dictionary.getIdf(tfEntety._1)*tfEntety._2))
+
+    val scores = docPool.
+      map(docId => (docId,postingLists.getLogTf(docId))).
+      map(tfMap => (tfMap._1,tfMap._2.filterKeys(term => queryVector.keySet.contains(term)))).
+      map(tfMap => (tfMap._1,tfMap._2.map(tfEntety => (tfEntety._1,dictionary.getIdf(tfEntety._1)*tfEntety._2)))).
+      map(tfidfMap => (tfidfMap._1,tfidfMap._2.map(tfidfEntety => queryVector.get(tfidfEntety._1).get*tfidfEntety._2).sum))
+
+    scores.sortBy(entety => 1/entety._2)
+  }
+}
